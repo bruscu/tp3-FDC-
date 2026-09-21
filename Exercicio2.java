c5f9d633acd4a83c3c9ca744b1dd1c1ece2c77d8
Bruno Santos <159336669+bruscu@users.noreply.github.com>
2026-09-21 10:37:31 -0300

    Create Produto class with product attributes
    Define a Produto class with attributes for name, price, and stock quantity.

---
+++ Exercicio2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+public class Produto{
+  public String nome;
+  public double preco;
+  public int quantidadeEmEstoque;
+}
+
+//O nome é um atributo importante para poder identificar o produto, o preco para saber o quanto custa o produto e quantidadeEmEstoque informa quantas quantidades 
+//daquele produto referente estão ainda em estoque.
