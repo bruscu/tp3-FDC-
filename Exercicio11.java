f804648bb079ad599c6f158b65ebda86daa4282f
Bruno Santos <159336669+bruscu@users.noreply.github.com>
2026-09-21 14:05:30 -0300

    Implement Circulo and Esfera classes
    Added classes for Circulo and Esfera with area and volume calculations.

---
+++ Exercicio11.java
@@ -0,0 +1,15 @@
+public class Circulo {
+  public double raio;
+
+  public double calcularArea(){
+    return Math.PI * (raio * raio);
+  }
+}
+
+public class Esfera {
+  public double raio;
+
+  public double calcularVolume(){
+    return (4.0 / 3.0) * Math.PI * (raior * raio * raio); 
+  }
+}
