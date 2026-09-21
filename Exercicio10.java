001d34d898abb9aac595c67cec62863886113278
Bruno Santos <159336669+bruscu@users.noreply.github.com>
2026-09-21 14:00:20 -0300

    Create Circulo and Esfera classes
    Added Circulo and Esfera classes with raio property.

---
+++ Exercicio10.java
@@ -0,0 +1,10 @@
+public class Circulo {
+  public double raio;
+}
+
+
+public class Esfera {
+  public double raio;
+}
+
+//O raio é necessário para calcular a área do círculo e o volume da esfera. 
